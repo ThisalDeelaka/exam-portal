0a56dd6e03967ca3bedcac2a8de10c66e0fbb54f
Thisal Deelaka <165908864+ThisalDeelaka@users.noreply.github.com>
2025-03-09 00:55:18 +0530

    fix tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
